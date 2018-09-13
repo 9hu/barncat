89cf2a3152817a823a384376ab45d9147949d376
dune <dune@hauntware.cc>
2018-09-12 20:14:34 -0700

    Refactor barncat heavily

--- config.sh
+++ config.sh
@@ -10,14 +10,14 @@ markdown_converter='kramdown'
 html_cleanup='tidy -q --show-warnings no -w 0 --tidy-mark no -m'
 
 # where are things located?
-# ensure it ends with a "/"
-input='./input/'
-output='./output/'
-layouts='./layouts/'
-cache='./.cache/'
+input='./input'
+output='./output'
+layouts='./layouts'
+cache='./.cache'
 
 # enforce a specific layout on files
 # when unspecified, layout defaults to default.html
-declare -A layout
+declare -a layout;
 # layout[path/to/files/]=layout.html
-layout[blog/]=blog-post.html
+layout+=(/blog/ blog-post.html)
+layout+=(/ default.html)
